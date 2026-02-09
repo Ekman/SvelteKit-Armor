5c5e774ed53d26d8aed731ef7ecc4e81c5f00573
Niklas Ekman <nikl.ekman@gmail.com>
2026-02-09 13:18:39 +0100

    proper session get

--- src/session/cookie.ts
+++ src/session/cookie.ts
@@ -1,6 +1,7 @@
 import {RequestEvent} from "@sveltejs/kit";
 import {COOKIE_TOKENS, cookieDelete, cookieGet, cookieSet} from "../utils/cookie";
 import {ArmorConfig, ArmorTokens} from "../contracts";
+import {ArmorAuthMissingError} from "../errors";
 
 export function armorCookieSessionExists({ cookies }: RequestEvent): boolean {
 	return Boolean(cookies.get(COOKIE_TOKENS));
@@ -14,8 +15,14 @@ export function armorCookieSessionLogout({ cookies }: RequestEvent): void {
 	cookieDelete(cookies, COOKIE_TOKENS);
 }
 
-export function armorCookieSessionGet({ cookies }: RequestEvent): void {
-	return cookieGet(cookies, COOKIE_TOKENS);
+export function armorCookieSessionGet({ cookies }: RequestEvent): ArmorTokens {
+	const tokens = cookieGet<ArmorTokens>(cookies, COOKIE_TOKENS);
+
+	if (!tokens) {
+		throw new ArmorAuthMissingError();
+	}
+
+	return tokens;
 }
 
 export const armorCookieSession: ArmorConfig['session'] = {
