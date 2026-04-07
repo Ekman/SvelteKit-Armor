c7fd3e33b3149324b53b1a588c181a6f2f0721ee
Niklas Ekman <niklas.ekman1@proton.me>
2026-04-07 16:44:14 +0200

    Fix event state validation logic

--- src/utils/event.ts
+++ src/utils/event.ts
@@ -5,5 +5,5 @@ export function eventStateValid(event: RequestEvent): boolean {
 	const state = event.url.searchParams.get("state") ?? undefined;
 	const stateCookie = cookieGetAndDelete(event.cookies, COOKIE_STATE);
 
-	return state !== stateCookie;
+	return state === stateCookie;
 }
